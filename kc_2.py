baea1d35d2a5074ab16e3ae6d2b39cd6ed3c539c
LilGotit <jbeld0002@kctcs.edu>
2022-10-31 01:46:15 -0400

    Switched ID to x variable after converting Injury_Date from object to datetime

--- kc_2.py
+++ kc_2.py
@@ -3,8 +3,10 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 dataframe = pd.read_csv("assets/fireInjuries.csv")
-ID = dataframe["ID"]
-Total_Injuries = dataframe["Total_Injuries"]
 
-plt.plot(ID, Total_Injuries, marker="^")
+Total_Injuries = dataframe["Total_Injuries"]
+Injury_Date = pd.to_datetime(dataframe["Injury_Date"], errors = "coerce")
+
+
+plt.plot(Injury_Date, Total_Injuries, marker="^")
 plt.show()
